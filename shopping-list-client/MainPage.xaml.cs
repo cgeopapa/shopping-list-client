6395b8879566bbf9c51568000be20784b41ef032
qaz wsx <cgeocodgod@gmail.com>
2020-03-12 16:19:56 +0200

    + Pull-to-Refresh

--- shopping-list-client/MainPage.xaml.cs
+++ shopping-list-client/MainPage.xaml.cs
@@ -2,22 +2,37 @@
 using System;
 using System.ComponentModel;
 using Xamarin.Forms;
-using System.Collections.Generic;
 using shopping_list_client.Controller;
+using System.Windows.Input;
 
 namespace shopping_list_client
 {
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        private RestfulController restfulController;
-        private List<Item> serverItems = new List<Item>();
-        
+        private readonly RestfulController restfulController;
+
+        private bool _isRefreshing = false;
+        public bool IsRefreshing
+        {
+            get { return _isRefreshing; }
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+        public ICommand RefreshCommand { get; }
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            itemsListView.RefreshCommand = RefreshCommand;
+            itemsListView.IsRefreshing = IsRefreshing;
 
             restfulController = new RestfulController();
+            RefreshCommand = new Command(Refresh);
 
             restfulController.items = restfulController.GetItems();
             itemsListView.ItemsSource = restfulController.items;
@@ -45,6 +60,17 @@ namespace shopping_list_client
         private void ToolbarItem_Clicked(object sender, EventArgs e)
         {
             restfulController.DeleteBought();
+        }
+
+        private void Refresh()
+        {
+            IsRefreshing = true;
+
+            itemsListView.ItemsSource = null;
+            restfulController.GetItems();
+            itemsListView.ItemsSource = restfulController.items;
+
+            IsRefreshing = false;
         }
     }
 }
